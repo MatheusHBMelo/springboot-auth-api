f6959b7ae5bf810c7c7ef988f36b51e84440882c
MatheusHBMelo <matheushbmelo@gmail.com>
2024-09-03 09:15:28 -0300

    Feat: add AuthenticationService class and its loadByUsername method

---
+++ src/main/java/com/theus/auth_api/user/service/AuthenticationService.java
@@ -0,0 +1,21 @@
+package com.theus.auth_api.user.service;
+
+import com.theus.auth_api.user.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username);
+    }
+}

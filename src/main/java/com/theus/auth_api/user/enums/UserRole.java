8f70946ab9c79960125219cd578f3d4143148598
MatheusHBMelo <matheushbmelo@gmail.com>
2024-09-03 09:12:22 -0300

    Feat: Add User class and UserRole Enum

---
+++ src/main/java/com/theus/auth_api/user/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.theus.auth_api.user.enums;
+
+public enum UserRole {
+    USER("USER"),
+    ADMIN("ADMIN");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+}
---
+++ src/main/java/com/theus/auth_api/user/model/User.java
@@ -0,0 +1,46 @@
+package com.theus.auth_api.user.model;
+
+import com.theus.auth_api.user.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("Role_Admin"), new SimpleGrantedAuthority("Role_User"));
+        return List.of(new SimpleGrantedAuthority("Role_User"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+}

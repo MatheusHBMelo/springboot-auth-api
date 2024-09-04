d8c5660fbb9a4fb9e77ec91ae8bab6325642078f
MatheusHBMelo <matheushbmelo@gmail.com>
2024-09-04 09:32:59 -0300

    Feat: Add TokenFilter class with OncePerRequestFilter

---
+++ src/main/java/com/theus/auth_api/infra/security/TokenFilter.java
@@ -0,0 +1,46 @@
+package com.theus.auth_api.infra.security;
+
+import com.theus.auth_api.user.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class TokenFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    public TokenFilter(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoveryToken(request);
+        if (token != null) {
+            var login = this.tokenService.validateToken(token);
+            UserDetails user = this.userRepository.findByUsername(login);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoveryToken(HttpServletRequest request) {
+        var tokenHeader = request.getHeader("Authorization");
+        if (tokenHeader != null) {
+            return tokenHeader.replace("Bearer ", "");
+        } else {
+            return null;
+        }
+    }
+}

03620bb57bb296a64981f69adffa856a69d201a9
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-14 13:31:22 -0300

    Refactor: Update the entity data types in the migration

--- src/main/resources/db/migration/V1__create-table-products.sql
+++ src/main/resources/db/migration/V1__create-table-products.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tb_products (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    price DOUBLE PRECISION NOT NULL,
-    description VARCHAR(255)
+    description VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
 );

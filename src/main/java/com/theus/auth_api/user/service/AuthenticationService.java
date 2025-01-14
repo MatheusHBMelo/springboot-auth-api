f05668dd012e4d11229947db748ad6a6100539df
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-14 13:35:15 -0300

    refactor: Update the entity for dependency injection with Lombok

--- src/main/java/com/theus/auth_api/user/service/AuthenticationService.java
+++ src/main/java/com/theus/auth_api/user/service/AuthenticationService.java
@@ -1,18 +1,18 @@
 package com.theus.auth_api.user.service;
 
 import com.theus.auth_api.user.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService implements UserDetailsService {
     private final UserRepository userRepository;
-
-    public AuthenticationService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {

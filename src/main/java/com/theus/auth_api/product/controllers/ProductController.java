f2c0eb683899b9e67201c61d90792ac2a1a3bdde
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-14 14:40:10 -0300

    feat: Add endpoints to find by ID, update, and delete a product

--- src/main/java/com/theus/auth_api/product/controllers/ProductController.java
+++ src/main/java/com/theus/auth_api/product/controllers/ProductController.java
@@ -1,32 +1,54 @@
 package com.theus.auth_api.product.controllers;
 
-import com.theus.auth_api.product.dto.ProductRequestDTO;
-import com.theus.auth_api.product.dto.ProductResponseDTO;
-import com.theus.auth_api.product.service.ProductService;
-import jakarta.validation.Valid;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.theus.auth_api.product.controllers.dto.ProductRequestDTO;
+import com.theus.auth_api.product.controllers.dto.ProductResponseDTO;
+import com.theus.auth_api.product.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> findAll() {
+        return ResponseEntity.ok(productService.findAll());
     }
 
-    @GetMapping
-    public ResponseEntity<List<ProductResponseDTO>> getProducts() {
-        return ResponseEntity.ok(this.productService.getProducts());
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<ProductResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.findById(id));
     }
 
     @PostMapping
-    public ResponseEntity<Void> saveProduct(@RequestBody @Valid ProductRequestDTO productRequestDTO) {
-        this.productService.saveProduct(productRequestDTO);
+    public ResponseEntity<Void> create(@RequestBody ProductRequestDTO productRequestDTO) {
+        productService.create(productRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<ProductResponseDTO> update(@PathVariable Long id, @RequestBody ProductRequestDTO productRequestDTO) {
+        return ResponseEntity.ok(productService.update(id, productRequestDTO));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }

e274f94b553330f0a2dc9a2529940911f3a33194
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-14 13:32:28 -0300

    Refactor: Update the entity data types

--- src/main/java/com/theus/auth_api/user/model/User.java
+++ src/main/java/com/theus/auth_api/user/model/User.java
@@ -9,18 +9,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "tb_users")
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     @Column(nullable = false, unique = true)
     private String username;
     @Column(nullable = false)

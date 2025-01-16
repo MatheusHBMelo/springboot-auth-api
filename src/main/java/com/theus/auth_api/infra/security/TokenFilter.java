034c199acbebeb9f829fc6243a03a1b40f1e6394
MatheusHBMelo <matheushbmelo@gmail.com>
2025-01-16 10:43:31 -0300

    feat: Change the dependency injection method and create two validations for expired tokens and null users

--- src/main/java/com/theus/auth_api/infra/security/TokenFilter.java
+++ src/main/java/com/theus/auth_api/infra/security/TokenFilter.java
@@ -1,10 +1,13 @@
 package com.theus.auth_api.infra.security;
 
 import com.theus.auth_api.user.repositories.UserRepository;
+import com.theus.auth_api.user.service.exceptions.TokenExpiratedException;
+import com.theus.auth_api.user.service.exceptions.UsernameIncorrectException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,27 +15,58 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.time.Instant;
 
 @Component
+@RequiredArgsConstructor
 public class TokenFilter extends OncePerRequestFilter {
     private final TokenService tokenService;
     private final UserRepository userRepository;
 
-    public TokenFilter(TokenService tokenService, UserRepository userRepository) {
-        this.tokenService = tokenService;
-        this.userRepository = userRepository;
-    }
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = recoveryToken(request);
-        if (token != null) {
-            var login = this.tokenService.validateToken(token);
-            UserDetails user = this.userRepository.findByUsername(login);
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        try {
+            if (token != null) {
+                var login = this.tokenService.validateToken(token);
+
+                if (login.isEmpty()) {
+                    throw new TokenExpiratedException("Access token has expired.");
+                }
+
+                UserDetails user = this.userRepository.findByUsername(login);
+
+                if (user == null) {
+                    throw new UsernameIncorrectException("User not found for the given token");
+                }
+
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        } catch (TokenExpiratedException ex) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write(
+                    String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"Token expired\", \"message\": \"%s\", \"path\": \"%s\"}",
+                            Instant.now(),
+                            HttpServletResponse.SC_UNAUTHORIZED,
+                            ex.getMessage(),
+                            request.getRequestURI()
+                    )
+            );
+        } catch (UsernameIncorrectException ex) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter().write(
+                    String.format("{\"timestamp\": \"%s\", \"status\": %d, \"error\": \"Unauthorized\", \"message\": \"%s\", \"path\": \"%s\"}",
+                            Instant.now(),
+                            HttpServletResponse.SC_UNAUTHORIZED,
+                            ex.getMessage(),
+                            request.getRequestURI()
+                    )
+            );
         }
-        filterChain.doFilter(request, response);
     }
 
     private String recoveryToken(HttpServletRequest request) {
